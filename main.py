8c2739bb86ee548a3a40930d27342f804e272885
unknown <alessandramost@gmail.com>
2025-11-02 11:41:24 +0100

    fix python

--- main.py
+++ main.py
@@ -96,9 +96,11 @@ def update_iot_data(data: IoTData):
 def root():
     return {"message": "Museo Smart System API attiva 🚀"}
 
-    if __name__ == "__main__":
+# ==========================
+# 🏁 START SERVER
+# ==========================
+if __name__ == "__main__":
     import os
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
-
